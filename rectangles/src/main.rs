45dcb49fd910dd4158fcb415f285ab3570afdb2e
ed cuss <edcussmusic@gmail.com>
2025-03-11 18:44:28 +0000

    feat: ch5 - rectangles

---
+++ rectangles/src/main.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let sq = Rectangle::square(20);
+    
+    dbg!(&rect1);
+    dbg!(&rect1.area());
+    dbg!(&sq);
+    dbg!(rect1.can_hold(&sq));
+
+}
+
+
