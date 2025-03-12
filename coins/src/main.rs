ff545e4754777c3d1947bc76a5d2ee089ba9b89d
ed cuss <edcussmusic@gmail.com>
2025-03-12 20:08:44 +0000

    feat: if let example

--- coins/src/main.rs
+++ coins/src/main.rs
@@ -6,6 +6,7 @@ enum SpecialDesign {
 }
 
 enum Coin {
+    Chocolate,
     OneP,
     TwoP,
     FiveP,
@@ -18,6 +19,7 @@ enum Coin {
 fn value_in_pence(item: &Option<Coin>) -> Option<u8> {
     match item {
         Some(coin) => match coin {
+            Coin::Chocolate => Some(0),
             Coin::OneP => Some(1),
             Coin::TwoP => Some(2),
             Coin::FiveP => Some(5),
@@ -34,9 +36,10 @@ fn value_in_pence(item: &Option<Coin>) -> Option<u8> {
 }
 
 fn main() {
-    let change = [
+    let change: [Option<Coin>; 6] = [
         Some(Coin::Pound),
         Some(Coin::TenP),
+        Some(Coin::Chocolate),
         Some(Coin::TwoP),
         None,
         Some(Coin::FiftyP(SpecialDesign::Olympic)),
@@ -44,9 +47,13 @@ fn main() {
 
     let mut total: u16 = 0;
     for coin in change {
-        let val = value_in_pence(&coin).unwrap_or(0);
-        println!("{val}");
-        total += val as u16;
+        if let Some(Coin::Chocolate) = coin {
+            println!("Whoops guess that wasn't a coin");
+        } else {
+            let val = value_in_pence(&coin).unwrap_or(0);
+            println!("{val}");
+            total += val as u16;
+        }
     }
     println!("total is {total}p");
     let pounds = (total as f32) / 100.0;
