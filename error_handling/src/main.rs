5e475af44d9a712cffae8e8ff9a14242a098841a
ed cuss <edcussmusic@gmail.com>
2025-03-16 16:57:47 +0000

    feat: most concise

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs;
 use std::io::{self, Read};
 
 fn main() {
@@ -6,7 +6,5 @@ fn main() {
 }
 
 fn read_str_from_file() -> Result<String, io::Error> {
-    let mut res = String::new();
-    File::open("hello.txt")?.read_to_string(&mut res)?;
-    Ok(res)
+    fs::read_to_string("hello.txt")
 }
