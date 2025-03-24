de522ce8f631606ff8920cd78ddd53318573b948
ed cuss <edcussmusic@gmail.com>
2025-03-24 20:43:31 +0000

    fix: eprintln for errors

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,14 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing args: {err}");
+        eprintln!("problem parsing args: {err}");
         process::exit(1);
     });
 
-    // dbg!(&config);
-
     if let Err(e) = minigrep::run(config) {
-        println!("app error: {e}");
+        eprintln!("app error: {e}");
         process::exit(1);
     }
 }
